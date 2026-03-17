99f045d2f6d9c05eccfa38027e805c41edb84cff
Calvin Francis <calvin.francis96@outlook.com>
2026-03-17 13:07:43 +0000

    Customize schema based on environment

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,8 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {% set env = env_var('DBT_ENV_NAME') %} -- DBT_ENV_NAME was created in Environment Settings
+    {%- if custom_schema_name is none or env = 'dev' -%} -- Must commit and deploy changes to prod before they take effect
 
         {{ default_schema }}
 
